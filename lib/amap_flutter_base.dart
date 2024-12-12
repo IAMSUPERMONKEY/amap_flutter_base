106d996772a6d4b06455f2d44d57163f083eb9c0
liwenbo <liwenbo@supermonkey.com.cn>
2024-12-12 15:13:23 +0800

    fix: fork 3.0版本，修改为 Object.hash

---
+++ lib/amap_flutter_base.dart
@@ -0,0 +1,13 @@
+library amap_flutter_base;
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+part 'src/amap_api_key.dart';
+part 'src/amap_privacy_statement.dart';
+part 'src/amap_tools.dart';
+part 'src/amap_utils.dart';
+part 'src/callbacks.dart';
+part 'src/location.dart';
+part 'src/poi.dart';
